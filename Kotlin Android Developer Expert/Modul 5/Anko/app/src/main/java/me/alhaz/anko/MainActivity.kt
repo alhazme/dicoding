3e8a21be63a69c3cce0400c9ca77ad6cbeaae211
Mochamad Fariz Al Hazmi <mas.aiz.azmi@gmail.com>
2019-01-04 21:26:09 +0700

    no message

--- Kotlin Android Developer Expert/Modul 5/Anko/app/src/main/java/me/alhaz/anko/MainActivity.kt
+++ Kotlin Android Developer Expert/Modul 5/Anko/app/src/main/java/me/alhaz/anko/MainActivity.kt
@@ -92,7 +92,6 @@ class MainActivity : AppCompatActivity() {
                 }.lparams(width = matchParent){
                     topMargin = dip(5)
                 }
-
             }
         }
     }
