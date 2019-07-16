5d0b6757db734f3bdef2b5ccb2a19beca5a13c46
Mochamad Fariz Al Hazmi <mas.aiz.azmi@gmail.com>
2019-07-16 14:33:26 +0700

    [INIT] Academies Project

---
+++ Belajar Android Jetpack Pro/Modul 1/Academies/app/src/main/java/me/alhaz/snippet/academies/detail/DetailCourseActivity.kt
@@ -0,0 +1,13 @@
+package me.alhaz.snippet.academies.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.alhaz.snippet.academies.R
+
+class DetailCourseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_course)
+    }
+}
---
+++ Belajar Android Jetpack Pro/Modul 1/Academies/app/src/main/java/me/alhaz/snippet/academies/reader/CourseReaderActivity.kt
@@ -0,0 +1,13 @@
+package me.alhaz.snippet.academies.reader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.alhaz.snippet.academies.R
+
+class CourseReaderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_reader)
+    }
+}
